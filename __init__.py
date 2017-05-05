a73ec7556dcae5ab45cf1efcaa284c584c99d010
Paul Phillips <mail@paulphillips.co.uk>
2017-05-05 23:06:47 +0100

    Update with Angular

--- __init__.py
+++ __init__.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, send_file
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/")
+def index():
+    return send_file("templates/index.html")
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
